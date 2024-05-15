9790a2b1d87d850b04e1527185b369533ff02a5f
Jeninson Peralta <jeninson@gmail.com>
2024-05-14 21:43:32 -0500

    Clases en PHP para Conectar a la Base de datos MD5.js

--- control/login.php
+++ control/login.php
@@ -1,23 +1,35 @@
 <?php
+require_once("configdb");
+
 //header('Content-Type: application/json');
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     try {
         $_post = json_decode(file_get_contents('php://input'),true);
         //var_dump($_post);
         if($_post["user"]!="" && $_post["pass"]!=""){
-            //echo $_post["user"];            
+            //echo $_post["user"]; 
+            $bd = new ConfigDb();
+            $conn = $bd->conexion();
+            /*$sql = "select 1";
+            $stmt = $conn ->prepare($sql);
+            if($stmt->execute()){
+                echo "SQL realizada correctamente";
+            }else{
+                echo "SQL no aplicada";
+            }*/
+            
             header("HTTP/1.1 200 OK");
-            echo json_encode(['msg' => 'Consulta finalizada correctamente']);
-            //http_response_code(200);
+            echo json_encode(['code'=>200,'msg' => 'Consulta finalizada correctamente']);
         }
-        exit();
+        //exit();
     } catch (Exception $ex) {
         header("HTTP/1.1 500");
-        echo json_encode(['msg' => 'Error, '.$ex->getMessage()]);
+        echo json_encode(['code'=>500,'msg' => 'Error interno al procesar su petici&oacute;n', "ERROR"=>$ex->getMessage()]);
+        //echo json_encode(['code'=>500,'msg' => 'Error interno al procesar su peticion'.$ex->getMessage()]);
     }
 }else {
     header("HTTP/1.1 400");
-    echo json_encode(['msg' => 'Error, La peticion no se pudo procesar']);
+    echo json_encode(['code'=>400,'msg' => 'Error, La peticion no se pudo procesar']);
 }
 
 ?>
--- js/login.js
+++ js/login.js
@@ -1,17 +1,21 @@
-const enviarLogin = async (url, method = "", param = undefined)=>{
-    if(param !== undefined && method==="GET") url += "?"+ new URLSearchParams(param)
-    if (method === "GET") method={method,headers: {'Content-Type':'application/json'}}    
+let divMsg = document.querySelector("#msgLogin")
+
+const enviarLogin = async (url="", method = "", param = undefined)=>{
+    //if(param !== undefined && method==="GET") url += "?"+ new URLSearchParams(param)
+    //if (method === "GET") method={method,headers: {'Content-Type':'application/json'}}    
     if (method === "POST") method={method,headers: {'Content-Type':'application/json'},body: JSON.stringify(param)}
-    if (method === "PUT") method={method,headers: {'Content-Type':'application/json'},body: JSON.stringify(param)}
-    if (method === "DELETE") method={method,headers: {'Content-Type':'application/json'},body: JSON.stringify(param)}
+    //if (method === "PUT") method={method,headers: {'Content-Type':'application/json'},body: JSON.stringify(param)}
+    //if (method === "DELETE") method={method,headers: {'Content-Type':'application/json'},body: JSON.stringify(param)}
     
+    divMsg.innerHTML = `<div class="spinner-border text-black" role="status"><span class="sr-only"></span></div>`
     fetch(url,method)
         .then(response =>response.json())
         .then(data=> validarLogin(data))
         .catch(e => console.log(e))  
 }
-const validarLogin = (data)=>{
-    console.log(data)
+const validarLogin = (info)=>{
+    (info.code===200) ? window.location.href="menu.html" : divMsg.innerHTML =`<b class='text-danger'>${info.msg}</b>`
+        
 }
 
 let form = document.getElementById("form_login")
@@ -19,19 +23,15 @@ let form = document.getElementById("form_login")
 form.addEventListener('submit', (e)=>{
     e.preventDefault()
     let user = document.getElementById("user").value
-    let pass = document.getElementById("pass").value
+    let pass = md5(document.getElementById("pass").value)
     let param = {user, pass}
-    //console.log(json)
-    //console.log(JSON.parse(json))
+    //console.log(param)
+    //console.log(JSON.stringify(param))
     
-    if(user!=="" && pass!==""){        
-        //alert("Estamos aqui... ok")
+    if(user!=="" && pass!==""){
         let method = "POST"
         //fetch('../control/login.php', {method: 'POST',body: JSON.stringify(param)})
         //fetch('../control/login.php?'+ new URLSearchParams(param), {method: 'GET'})
         enviarLogin("../control/login.php",method,param)
     }
 })
-
-
-
